b9a2a4eef3436d0f45c466297c3594baa204ecbc
wqj <3167284533@qq.com>
2020-08-27 20:23:06 +0800

    纯js 卡片式轮播  堆叠式轮播

---
+++ js/test.js
@@ -0,0 +1,28 @@
+
+
+// function fn1() {
+//     console.log(1)
+//     // console.log(this)
+//     this();//fn2()
+// }
+
+// function fn2() {
+//     console.log(this)
+//     console.log(2)
+// }
+
+
+// // fn1();
+// fn1.call();
+// fn1.call(fn2);
+// fn1.call.call(fn2)
+
+
+
+let arr = [1, 2.47, 65, 78, [43, 6, 8, [6, 9, 7]]];
+
+function flatten(arr) {
+    arr.forEach((i)=>{
+
+    })
+}
